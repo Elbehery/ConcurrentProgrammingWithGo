f9fd0d49d9c00eb8d44b2598b8c57351aab6dc8d
Mustafa Elbehery <melbeher@redhat.com>
2023-08-09 10:49:02 +0200

    ex2.3

---
+++ mych2/ex2.3.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+)
+
+func GrepDir(path, pattern string, file os.DirEntry, wg *sync.WaitGroup) {
+	fullPath := filepath.Join(path, file.Name())
+	if !file.IsDir() {
+		f, err := os.ReadFile(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.Contains(string(f), pattern) {
+			fmt.Println(fullPath, "contains a match with", pattern)
+		} else {
+			fmt.Println(fullPath, "does NOT contain a match with", pattern)
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	pat := os.Args[1]
+	path := os.Args[2]
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+	for _, f := range files {
+		wg.Add(1)
+		go GrepDir(path, pat, f, &wg)
+	}
+	wg.Wait()
+}

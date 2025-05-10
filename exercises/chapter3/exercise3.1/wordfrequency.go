808f8e26a4121fb986b6bdcc8a6122c30cd1e897
Mustafa Elbehery <melbeher@redhat.com>
2025-05-11 01:00:38 +0200

    mutex to map
    Signed-off-by: Mustafa Elbehery <melbeher@redhat.com>

--- exercises/chapter3/exercise3.1/wordfrequency.go
+++ exercises/chapter3/exercise3.1/wordfrequency.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -15,7 +16,7 @@ Additionally we have a timer at the end which you might need to adjust
 depending on how fast your internet connection is.
 In later chapters we cover how to wait for threads to complete their work
 */
-func countLetters(url string, frequency map[string]int) {
+func countLetters(url string, frequency map[string]int, mutex *sync.Mutex) {
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	if resp.StatusCode != 200 {
@@ -23,21 +24,26 @@ func countLetters(url string, frequency map[string]int) {
 	}
 	body, _ := io.ReadAll(resp.Body)
 	wordRegex := regexp.MustCompile(`[a-zA-Z]+`)
+	mutex.Lock()
 	for _, word := range wordRegex.FindAllString(string(body), -1) {
 		wordLower := strings.ToLower(word)
 		frequency[wordLower] += 1
 	}
+	mutex.Unlock()
 	fmt.Println("Completed:", url)
 }
 
 func main() {
+	mutex := sync.Mutex{}
 	var frequency = make(map[string]int)
 	for i := 1000; i <= 1020; i++ {
 		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
-		go countLetters(url, frequency)
+		go countLetters(url, frequency, &mutex)
 	}
 	time.Sleep(10 * time.Second)
+	mutex.Lock()
 	for k, v := range frequency {
 		fmt.Println(k, "->", v)
 	}
+	mutex.Unlock()
 }

b7cb34234310e25080250c2817e04b8fda3c6e26
Mustafa Elbehery <melbeher@redhat.com>
2025-05-15 14:09:45 +0200

    add RWLock with writer preferred
    Signed-off-by: Mustafa Elbehery <melbeher@redhat.com>

--- mych5/ReadWriterLock.go
+++ mych5/ReadWriterLock.go
@@ -7,53 +7,51 @@ import (
 )
 
 type ReadWriteMutex struct {
-	readerCounter, writersWaiting int
-	activeWriter                  bool
-	cond                          *sync.Cond
+	currentReaders, waitingWriters int
+	activeWriter                   bool
+	cond                           *sync.Cond
 }
 
 func NewReadWriteMutex() *ReadWriteMutex {
 	return &ReadWriteMutex{
-		cond: &sync.Cond{
-			L: &sync.Mutex{},
-		},
+		cond: sync.NewCond(&sync.Mutex{}),
 	}
 }
 
-func (rwm *ReadWriteMutex) RLock() {
-	rwm.cond.L.Lock()
-	if rwm.activeWriter || rwm.writersWaiting > 0 {
-		rwm.cond.Wait()
+func (m *ReadWriteMutex) Lock() {
+	m.cond.L.Lock()
+	m.waitingWriters++
+	for m.currentReaders > 0 || m.activeWriter {
+		m.cond.Wait()
 	}
-	rwm.readerCounter++
-	rwm.cond.L.Unlock()
+	m.waitingWriters--
+	m.activeWriter = true
+	m.cond.L.Unlock()
 }
 
-func (rwm *ReadWriteMutex) RUnLock() {
-	rwm.cond.L.Lock()
-	rwm.readerCounter--
-	if rwm.readerCounter == 0 {
-		rwm.cond.Broadcast()
-	}
-	rwm.cond.L.Unlock()
+func (m *ReadWriteMutex) UnLock() {
+	m.cond.L.Lock()
+	m.activeWriter = false
+	m.cond.Broadcast()
+	m.cond.L.Unlock()
 }
 
-func (rwm *ReadWriteMutex) Lock() {
-	rwm.cond.L.Lock()
-	rwm.writersWaiting++
-	if rwm.activeWriter || rwm.readerCounter > 0 {
-		rwm.cond.Wait()
+func (m *ReadWriteMutex) RLock() {
+	m.cond.L.Lock()
+	for m.activeWriter || m.waitingWriters > 0 {
+		m.cond.Wait()
 	}
-	rwm.writersWaiting--
-	rwm.activeWriter = true
-	rwm.cond.L.Unlock()
+	m.currentReaders++
+	m.cond.L.Unlock()
 }
 
-func (rwm *ReadWriteMutex) UnLock() {
-	rwm.cond.L.Lock()
-	rwm.activeWriter = false
-	rwm.cond.Broadcast()
-	rwm.cond.L.Unlock()
+func (m *ReadWriteMutex) RUnLock() {
+	m.cond.L.Lock()
+	m.currentReaders--
+	if m.currentReaders == 0 {
+		m.cond.Broadcast()
+	}
+	m.cond.L.Unlock()
 }
 
 func main() {

34f196e317dbac6bbd4a5c83161be033fca65a14
Mustafa Elbehery <melbeher@redhat.com>
2025-05-18 16:57:38 +0200

    add 6.1 wg

--- mych6/main.go
+++ mych6/main.go
@@ -2,97 +2,25 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"sync"
+	"time"
 )
 
-type WaitGrp struct {
-	size int
-	cond *sync.Cond
-}
-
-func NewWaitGrp() *WaitGrp {
-	return &WaitGrp{cond: sync.NewCond(&sync.Mutex{})}
-}
-
-func (wg *WaitGrp) Add(delta int) {
-	wg.cond.L.Lock()
-	wg.size += delta
-	wg.cond.L.Unlock()
-}
-
-func (wg *WaitGrp) Wait() {
-	wg.cond.L.Lock()
-	for wg.size > 0 {
-		wg.cond.Wait()
-	}
-	wg.cond.L.Unlock()
-}
-
-func (wg *WaitGrp) Done() {
-	wg.cond.L.Lock()
-	wg.size--
-	if wg.size == 0 {
-		wg.cond.Broadcast()
-	}
-	wg.cond.L.Unlock()
-}
-
-// MyWaitGroup is a wait-group implementation based on Semaphore.
-type MyWaitGroup struct {
-	s *sema
-}
-
-func NewMyWaitGroup(n int) *MyWaitGroup {
-	return &MyWaitGroup{newSema(1 - n)}
-}
-
-func (wg *MyWaitGroup) Wait() {
-	wg.s.acquire()
-}
-
-func (wg *MyWaitGroup) Done() {
-	wg.s.release()
-}
-
-// sema is to be used internally by MyWaitGroup type.
-type sema struct {
-	permits int
-	cond    *sync.Cond
-}
-
-func newSema(n int) *sema {
-	return &sema{
-		permits: n,
-		cond:    sync.NewCond(&sync.Mutex{}),
-	}
-}
-
-func (s *sema) acquire() {
-	s.cond.L.Lock()
-	for s.permits <= 0 {
-		s.cond.Wait()
-	}
-	s.permits--
-	s.cond.L.Unlock()
-}
-
-func (s *sema) release() {
-	s.cond.L.Lock()
-	s.permits++
-	s.cond.Signal()
-	s.cond.L.Unlock()
+func DoWork(id int, wg *sync.WaitGroup) {
+	n := rand.Intn(5)
+	time.Sleep(time.Duration(n) * time.Second)
+	fmt.Println(id, "Done after", n, " seconds")
+	wg.Done()
 }
 
 func main() {
-	wg := NewMyWaitGroup(4)
-	for i := 1; i <= 4; i++ {
-		go doWork(i, wg)
+	wg := sync.WaitGroup{}
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go DoWork(i, &wg)
 	}
-	wg.Wait()
-	fmt.Println("All complete")
-}
 
-func doWork(id int, wg *MyWaitGroup) {
-	fmt.Println(id, "Done working ")
-	wg.Done()
+	wg.Wait()
+	fmt.Println("ALl Done !!!")
 }

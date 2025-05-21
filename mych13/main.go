e3b31212c6510c4e61e56b9fcc814ed155aa65d6
Mustafa Elbehery <melbeher@redhat.com>
2025-05-21 15:07:42 +0200

    add spinlock

---
+++ mych13/main.go
@@ -0,0 +1,29 @@
+package mych13
+
+import (
+	"runtime"
+	"sync/atomic"
+)
+
+type Locker interface {
+	Lock()
+	UnLock()
+}
+
+type SpinLock struct {
+	val int32
+}
+
+func (s *SpinLock) Lock() {
+	for !atomic.CompareAndSwapInt32(&s.val, 0, 1) {
+		runtime.Gosched()
+	}
+}
+
+func (s *SpinLock) UnLock() {
+	atomic.StoreInt32(&s.val, 0)
+}
+
+func NewSpinLock() Locker {
+	return &SpinLock{val: int32(0)}
+}
